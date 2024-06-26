bf383a3a1b7abd72481f6de515e9d662699147de
Logon Aluno <logonrmlocal@fiap.com.br>
2024-06-26 13:24:10 -0300

    reset

--- src/components/Detalhes/Navbar2.jsx
+++ src/components/Detalhes/Navbar2.jsx
@@ -1,13 +1,12 @@
 import { Link } from "react-router-dom";
 import { FaArrowLeft } from 'react-icons/fa';
-import Logo from "../../../public/logo-otbm.png";
 
 export default function Navbar2() {
     return(
         <>
         <div className="flex justify-around items-center py-5">
-                <img src={Logo} alt="" />
-                <Link to="/">
+                <img src="../../public/logo-otbm.png" alt="" />
+                <Link to="../">
                 <div className="text-white text-3xl">
                     <FaArrowLeft/>
                 </div>
